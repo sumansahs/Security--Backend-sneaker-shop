870b8905c1fbf1cb520ea8814774713385cd06ce
sumansahs <sumansah900@gmail.com>
2024-08-11 15:31:45 +0545

    compare passwrod

--- model/usermodel.js
+++ model/usermodel.js
@@ -66,7 +66,7 @@ userSchema.pre('save', async function (next) {
     next();
 });
 
-// Method to compare passwords during login
+// Method to compare passwords during login compare
 userSchema.methods.comparePassword = async function (candidatePassword) {
     return bcrypt.compare(candidatePassword, this.password);
 };
